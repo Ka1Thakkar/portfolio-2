ce33678d69118c7954569e723631d71c1eed1037
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-05-28 18:46:57 +0530

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Nav() {
     return (
-        <nav className='py-0 sm:py-5 sm:px-40 sm:flex sm:justify-center md:justify-between sm:gap-2 bg-black sticky top-0 text-white font-mono'>
+        <nav className='py-0 sm:py-5 sm:px-40 md:flex md:justify-center lg:justify-between md:gap-2 bg-black sticky top-0 text-white font-mono'>
             <h1 className="text-opacity-100 md:text-opacity-100 md:text-3xl font-mono font-bold pt-1 sm:py-0 text-center">Ka1_Thakkar</h1>
             <h1 className="text-transparent sm:text-opacity-100 md:text-transparent text-sm py-o.5">|</h1>
             <ul className='flex pb-1 gap-1 md:gap-2 justify-center'>
