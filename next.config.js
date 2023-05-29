7197efe1664aed0aa6e469b6f52c7daf686d2b6c
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-05-29 07:57:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'drive.google.com',
-            port: '',
-            pathname: '',
-          },
-        ],
-      },
-}
+module.exports = nextConfig
