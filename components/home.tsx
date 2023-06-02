bd27c4e940f5861a06d873d774c745b4f65eb8aa
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-06-02 15:39:11 +0530

    Update home.tsx

--- components/home.tsx
+++ components/home.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             <h3 className='text-3xl pt-1 pb-5 md:text-3xl xl:text-6xl font-bold font-mono text-gray-400'>I am a Full Stack Developer and a Designer,</h3>
             <p className='text-xl pt-2 pb-10 text-gray-500 md:text-md xl:text-md max-w-xl font-mono rounded-xl'>Third year student at Birla Institute of Technology and Science, Pilani and a front-end development enthusiast. {"Let's"} connect!</p>
             <button className=" 2xl:bg-transparent border-4 border-orange-500 bg-orange-500 text-black 2xl:border-white 2xl:hover:border-orange-500 2xl:text-white py-1 px-5 rounded-lg text-xl font-bold mt-3 font-mono hover:shadow-md 2xl:hover:shadow-gray-600 2xl:hover:bg-orange-500 2xl:hover:shadow-md 2xl:hover:text-black flex items-center justify-center">
-                <a href="https://drive.google.com/file/d/1-dvGcmOb8DlGOP65GmR-2jjrbf_TUSuX/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='flex' >Resume 
+                <a href="https://drive.google.com/file/d/1-dvGcmOb8DlGOP65GmR-2jjrbf_TUSuX/view?usp=sharing" target="_blank" rel="noopener noreferrer" className='flex justify-center items-center' >Resume 
                     <BsArrowRightShort className='' />
                 </a>    
             </button>
